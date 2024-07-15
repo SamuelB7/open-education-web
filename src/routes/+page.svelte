9f46592138d8956b43539da0c1f7df7eb79f6225
Samuel Belo <belo.samuel@gmail.com>
2024-07-15 10:44:59 -0300

    simple integration with api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let hello = "";
+
+    onMount(async () => {
+        const res = await fetch("http://localhost:3333/hello-world");
+        hello = await res.text();
+        console.log("RESULT", hello);
+    });
+
+    export { hello };
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<p>{hello}</p>
