e96a3c786949ba114a222e76a07314306fcacc32
Samuel Belo <belo.samuel@gmail.com>
2024-09-02 15:40:28 -0300

    material ui and routing config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './pages/About.tsx'
+import Home from './pages/Home.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+
+
+export default function About() {
+
+    return (
+        <>
+            <h1>ABOUT PAGE</h1>
+        </>
+    )
+}
+
+
